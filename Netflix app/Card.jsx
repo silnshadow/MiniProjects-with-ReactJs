821121153d38a063492f41619176c5f9d2e1a1fe
Prashant Singh <havtohell1857@gmail.com>
2020-11-02 09:30:35 +0530

    We need to give different output in different div

--- Netflix app/Card.jsx
+++ Netflix app/Card.jsx
@@ -19,7 +19,8 @@ function Card(props){
         </div>
         <div className="aboutSeries">
             <h2>About</h2>
-            <p>Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.
+            {/*This code needs to be changed */}
+            <p >Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.
 
 You can watch as much as you want, whenever you want,
 without a single ad – all for one low monthly price.
