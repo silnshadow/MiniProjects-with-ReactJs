f8bea601115b8caddee6c99ced561ba3e4c24b96
Prashant Singh <havtohell1857@gmail.com>
2020-11-02 09:12:10 +0530

    Added Netflix app project source code

---
+++ Netflix app/Sdata.jsx
@@ -0,0 +1,29 @@
+const Sdata = [
+   { 
+    imgsrc: "https://wallpapercave.com/wp/wp4056397.jpg",
+    title: "A Netflix Orginal Series",
+    sname: "DARK",
+    link: "https://www.netflix.com/in/title/80100172"
+    },
+
+   {
+    imgsrc: "https://wallpapercave.com/fuwp/uwp52673.jpeg",
+    title: "A Netflix Orginal Series",
+    sname: "STRANGER THINGS",
+    link: "https://www.netflix.com/in/title/80057281"
+    },
+
+   { 
+    imgsrc: "https://wallpapercave.com/wp/wp1935738.jpg",
+    title: "A Netflix Orginal Series",
+    sname: "13 REASONS WHY",
+    link: "https://www.netflix.com/in/title/80117470?preventIntent: true"},
+
+   { 
+    imgsrc: "https://wallpapercave.com/wp/wp5152709.png",
+    title: "A Netflix Orginal Series",
+    sname: "SEX EDUCATION",
+    link: "https://www.netflix.com/in/title/80197526"}
+   ];
+
+   export default Sdata;
