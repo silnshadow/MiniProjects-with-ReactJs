0b7d101b0a2d043dafa4c1eb014d053d99a86660
Prashant Singh <havtohell1857@gmail.com>
2020-11-11 09:38:25 +0530

    In this project we will create template for three languages

---
+++ LayoutChanger/layoutchanger/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+          Learn React
+    </div>
+  );
+}
+
